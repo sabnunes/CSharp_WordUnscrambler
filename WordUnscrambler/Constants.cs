694ca50b1448cae0dafc9b22a39812ecb44bb185
sabnunes <sabrina.nunes.64@gmail.com>
2023-02-08 12:20:45 -0800

    adding word list and tidying up lines

--- WordUnscrambler/Constants.cs
+++ WordUnscrambler/Constants.cs
@@ -3,17 +3,17 @@ namespace WordUnscrambler
 {
     class Constants
     {
-        public const string OptionsOnHowToEnterScrambledWords = "Enter scrambled word(s) manually or as a file: F - file/M - manual";
+        public const string OptionsOnHowToEnterScrambledWords = "Enter scrambled word(s) manually or as a file. Type F for file and M for manual.";
         public const string OptionsOnContinuingTheProgram = "Would you like to continue? Y/N";
         
         public const string EnterScrambledWordsViaFile = "Enter full path including the file name: ";
         public const string EnterScrambledWordsManually = "Enter word(s) manually (separated by commas if multiple): ";
-        public const string EnterScrambledWordsOptionNotRecognized = "The option was not recognized.";
+        public const string EnterScrambledWordsOptionNotRecognized = "The option was not recognized.\n";
         public const string ErrorScrambledWordsCannotBeLoaded = "Scrambled words were not loaded because there was an error: ";
         public const string ErrorProgramWillBeTerminated = "The program will be terminated.";
         
-        public const string MatchFound = "MATCH FOUND FOR {0}: {1}";
-        public const string MatchNotFound = "NO MATCHES HAVE BEEN FOUND. ";
+        public const string MatchFound = "MATCH FOUND FOR {0}: {1}\n";
+        public const string MatchNotFound = "NO MATCHES HAVE BEEN FOUND. \n";
 
         public const string Yes = "Y";
         public const string No = "N";
